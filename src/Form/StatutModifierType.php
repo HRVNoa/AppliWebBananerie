e41bf7222a2292bdf22fcdefb76f347bf3c3426e
Paul Cuquemelle <paulcuquemelle@orange.fr>
2024-01-23 14:14:13 +0100

    Ajout bouton confirmer

--- src/Form/StatutModifierType.php
+++ src/Form/StatutModifierType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Statut;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,10 @@ class StatutModifierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
+            ->add('libelle',TextType::class , ["attr" => ["class" => "text-white"]])
         ;
+        $builder->add('enregistrer',SubmitType::class, array('label' => 'Nouveau Independant', "attr" => ["class" => "btn btn-primary"]));
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
