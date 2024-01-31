ab0d808212093f13ab4f03e732535a547807ce26
HRVNoa <hervieu.noa@gmail.com>
2024-01-31 10:54:42 +0100

    Test si l'utilisateur est connecter ou non pour le faire arrivé sur le dashboard

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    public function index(): Response
+    public function index(Security $security): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        if ($security->getUser()){
+            return $this->render('index/index.html.twig', [
+                'controller_name' => 'IndexController',
+            ]);
+        }
+        return $this->redirectToRoute('app_login');
     }
     public function choix(): Response
     {
